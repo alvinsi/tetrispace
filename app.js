b9a8759cde59381ef2b48e6ea95326f2ed53ed38
alvinleolopolisa@gmail.com <alvinleolopolisa@gmail.com>
2015-10-10 18:20:38 -0400

    added middleware

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us!');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function(){
